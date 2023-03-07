5aaed016065b29321399f99f2eafde2e7174000a
TahirBayraktar <111084545+TahirBayraktar@users.noreply.github.com>
2023-03-07 15:57:40 +0300

    UploadV2

--- InstagramFirebase/UploadViewController.swift
+++ InstagramFirebase/UploadViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 import FirebaseStorage
-      
+import FirebaseFirestore
+import FirebaseAuth
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
@@ -35,6 +36,12 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate & 
         self.dismiss(animated: true)
     }
     
+    func makeAlert(inputTittle:String,inputMessage:String){
+        let alert = UIAlertController(title: inputTittle, message:inputMessage, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        present(alert, animated: true)
+    }
     
     @IBAction func uploadClicked(_ sender: Any) {
 //        dosya referansı oluştur
@@ -42,20 +49,41 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate & 
         let storageReference = storage.reference()
         
         let mediaFolder = storageReference.child("media")
+//      kullanıcı birden fazla fotoğraf yüklemesi için id veriyoruz
+        let uuid = UUID().uuidString
 //        image->data'ya dönüştürme
         if let data = imageView.image?.jpegData(compressionQuality: 0.5){
             
-            let imageReference = mediaFolder.child("image.jpg")
+            let imageReference = mediaFolder.child("\(uuid).jpg")
             imageReference.putData(data) { metadata, error in
                 
                 if error != nil{
-                    print(error!.localizedDescription)
+                    self.makeAlert(inputTittle: "Error!", inputMessage: error?.localizedDescription ?? "Error!")
                 }else{
                     imageReference.downloadURL { url, error in
                         
                         if error == nil{
                             let imageUrl = url?.absoluteString
-                            print(imageUrl!)
+//                            DATABASE
+                            
+                            let firestoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl ?? "1234" , "postedBy" : Auth.auth().currentUser!.email! , "postComment" : self.commentText.text! , "date" : FieldValue.serverTimestamp() , "Likes" : 0] as [String:Any]
+                            
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost,completion: { error in
+                                
+                                if error != nil {
+                                    self.makeAlert(inputTittle: "Error!", inputMessage: error?.localizedDescription ?? "Error" )
+                                }else{
+                                    self.imageView.image = UIImage(named: "selectimages")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
+                                    
+                                }
+                            })
+                            
                         }
                     }
                     

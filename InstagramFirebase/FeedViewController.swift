dd1df235b9e2fc6a80d33a42be201fd4583dd181
TahirBayraktar <111084545+TahirBayraktar@users.noreply.github.com>
2023-04-16 14:58:31 +0300

    .

--- InstagramFirebase/FeedViewController.swift
+++ InstagramFirebase/FeedViewController.swift
@@ -6,26 +6,90 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseCore
+import SDWebImage
 
 class FeedViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    var documentArrayId = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getDataFromFirebase()
     }
+    
+    func getDataFromFirebase(){
+        
+        let fireStoreDatabase = Firestore.firestore()
+        
+       /* --->Eski sürüm tarih hatası çözümü
+        let setting = fireStoreDatabase.settings
+        setting.areTimestampsInSnapshotsEnabled = true
+        fireStoreDatabase.settings = setting
+        */
+        
+        fireStoreDatabase.collection("Posts").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
+            
+            if error != nil{
+                print(error?.localizedDescription ?? "Error!")
+            }else{
+                if snapshot?.isEmpty != true  && snapshot != nil{
+                    
+                    self.userEmailArray.removeAll(keepingCapacity: false)
+                    self.userCommentArray.removeAll(keepingCapacity: false)
+                    self.likeArray.removeAll(keepingCapacity: false)
+                    self.userImageArray.removeAll(keepingCapacity: false)
+                    self.documentArrayId.removeAll(keepingCapacity: false)
+                    
+                    for document in snapshot!.documents{
+                        let documentID = document.documentID
+                        self.documentArrayId.append(documentID)
+                        
+                        if let postedBy = document.get("postedBy") as? String{
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String{
+                            self.userImageArray.append(imageUrl)
+                           
+                        }
+                        if let likes = document.get("Likes") as? Int{
+                            self.likeArray.append(likes)
+                        }
+                        self.tableView.reloadData()
+                     }
+                }
+            }
+            
+        }
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
-        cell.userEmailLabel.text = "user@gmail.com"
-        cell.likeLabel.text = "0"
-        cell.commentLabel.text = "comment"
-        cell.userImageView.image = UIImage(named: "selectimages")
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.userImageView.sd_setImage(with: URL(string: self.userImageArray[indexPath.row]))
+        cell.documentIdLabel.text = documentArrayId[indexPath.row]
+        
         
         return cell
     }
